d7c6e95bbd1e83105b9c6f47be8a8da38a2b7115
Andrés <lopez.andresrigoberto@gmail.com>
2023-01-30 17:46:03 -0600

    [add]  home page component & styles

---
+++ src/routes/home/Home.component.jsx
@@ -0,0 +1,12 @@
+import Presentation from '../../components/sections/Presentation.component';
+import { HomePage } from './home.styles';
+
+const Home = () => {
+  return (
+    <HomePage>
+      <Presentation />
+    </HomePage>
+  );
+};
+
+export default Home;
