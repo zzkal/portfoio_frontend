b373fc76b5d2c9240e360972c1ae82d556028594
Andrés <lopez.andresrigoberto@gmail.com>
2023-01-31 20:03:16 -0600

    [add] about section to home

--- src/routes/home/Home.component.jsx
+++ src/routes/home/Home.component.jsx
@@ -1,10 +1,12 @@
 import Presentation from '../../components/sections/Presentation.component';
+import About from '../../components/sections/About.component';
 import { HomePage } from './home.styles';
 
 const Home = () => {
   return (
     <HomePage>
       <Presentation />
+      <About />
     </HomePage>
   );
 };
