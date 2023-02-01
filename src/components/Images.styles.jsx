9c665de38bbe130597ae63c683a4e88a6c65a257
Andrés <lopez.andresrigoberto@gmail.com>
2023-01-31 20:05:24 -0600

    [add] image component

---
+++ src/components/Images.styles.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const MainImage = styled.img`
+  border-radius: 30px;
+  align-self: center;
+  margin-top: 20rem;
+  width: 20rem;
+  box-shadow: -7px 8px 21px -10px rgba(0, 0, 0, 0.69);
+  -webkit-box-shadow: -7px 8px 21px -10px rgba(0, 0, 0, 0.69);
+  -moz-box-shadow: -7px 8px 21px -10px rgba(0, 0, 0, 0.69);
+`;
