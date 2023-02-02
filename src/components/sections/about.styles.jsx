ee9ac39920ca25b0907cd431a165bf30f9ee602e
Andrés <lopez.andresrigoberto@gmail.com>
2023-02-02 17:12:04 -0600

    [edit] style

--- src/components/sections/about.styles.jsx
+++ src/components/sections/about.styles.jsx
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
 export const SectionContainer = styled.div`
+  position: relative;
   display: flex;
   flex-flow: column;
-  position: relative;
-  padding: 0 4rem 0 4rem;
-  margin-top: 8rem;
+  justify-content: space-around;
+  padding: 8rem 4rem 10rem 4rem;
 `;
