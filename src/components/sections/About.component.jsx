54cc70f1a1700c34920f287eeb6e720dae7d92fc
Andrés <lopez.andresrigoberto@gmail.com>
2023-02-02 17:13:01 -0600

    [wip] on scroll & onvisibility

--- src/components/sections/About.component.jsx
+++ src/components/sections/About.component.jsx
@@ -1,22 +1,58 @@
-import { MainImage, MainCover } from '../Images.styles';
+import { MainImage } from '../Images.styles';
 import { SimpleParagraph } from '../paragraphs.styles';
 import { Title1 } from '../Titles.component';
 import { SectionContainer } from './about.styles';
 
 import Image1 from '../../assets/img/img1.jpg';
+import { Fragment, useEffect, useRef, useState } from 'react';
 
 const About = () => {
+  const [t, setTop] = useState(0);
+  const secRef = useRef(null);
+  const [scrollTop, setScrollTop] = useState(0);
+  const [firstTimeShown, setFirstTimeShown] = useState(true);
+  const [startAnimation, setStartAnimation] = useState(false);
+
+  // secRef.current.getBoundingClientRect()
+
+  useEffect(() => {
+    const { y } = secRef.current.getBoundingClientRect();
+    setTop(y);
+  }, []);
+
+  useEffect(() => {
+    const handleScroll = (e) => {
+      const { y } = secRef.current.getBoundingClientRect();
+
+      if ((y < 720) & (y > 0) & firstTimeShown) {
+        setFirstTimeShown(false);
+        setStartAnimation(true);
+        console.log('Animate');
+      }
+      console.log(y);
+      setScrollTop(window.scrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [scrollTop]);
+
   return (
-    <SectionContainer>
-      <Title1>About me</Title1>
-      <SimpleParagraph>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio,
-        voluptates sunt. Consequatur corporis accusantium laudantium sapiente
-        tempore vel, neque autem eum esse pariatur, quia hic! Ab eum nesciunt
-        non quos?
-      </SimpleParagraph>
-      <MainImage src={Image1} />
-    </SectionContainer>
+    <Fragment>
+      <SectionContainer ref={secRef}>
+        <Title1 animate={startAnimation}>About me</Title1>
+        <SimpleParagraph animate={startAnimation}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio,
+          voluptates sunt. Consequatur corporis accusantium laudantium sapiente
+          tempore vel, neque autem eum esse pariatur, quia hic! Ab eum nesciunt
+          non quos?
+        </SimpleParagraph>
+        <MainImage src={Image1} animate={startAnimation} />
+      </SectionContainer>
+    </Fragment>
   );
 };
 
