fc9d18d8eaf22df939c8dab3cb23cbd6bf001a5c
Andrés <lopez.andresrigoberto@gmail.com>
2023-02-03 17:51:13 -0600

    [add] paragraph 2

--- src/components/paragraphs.styles.jsx
+++ src/components/paragraphs.styles.jsx
@@ -1,13 +1,12 @@
 import styled, { keyframes } from 'styled-components';
+import { Colors } from '../styles/variables';
 
 const show = keyframes`
 from{
-  transform: translateY(20rem);
   opacity: 0;
 }
 to {
   opacity: 1;
-  transform: translateY(0);
 }
 `;
 
@@ -15,5 +14,14 @@ export const SimpleParagraph = styled.p`
   font-weight: 400;
   font-size: 2rem;
   padding-top: 8rem;
+  color: ${Colors.blueDark};
   animation: ${(props) => (props.animate ? show : '')} 0.8s ease-in-out;
+`;
+
+export const Paragraph2 = styled.p`
+  font-size: 1.5rem;
+  font-weight: 600;
+  padding-top: 5rem;
+  color: ${Colors.blueLight};
+  animation: ${(props) => (props.animate ? show : '')} 1s ease-in-out;
 `;
