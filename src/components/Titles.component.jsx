354478db6b09d6d3f1cf73bafd4639888bcdf10f
Andrés <lopez.andresrigoberto@gmail.com>
2023-02-03 17:50:47 -0600

    [add] titles types

--- src/components/Titles.component.jsx
+++ src/components/Titles.component.jsx
@@ -3,12 +3,10 @@ import { Colors } from '../styles/variables';
 
 const show = keyframes`
 from{
-  transform: translateY(10rem);
   opacity: 0;
 }
 to {
   opacity: 1;
-  transform: translateY(0);
 }
 `;
 
@@ -18,4 +16,21 @@ export const Title1 = styled.h1`
   font-size: 5rem;
   color: ${Colors.blueDark};
   animation: ${(props) => (props.animate ? show : '')} 0.8s ease-in-out;
+`;
+
+export const Title2 = styled.h2`
+  font-size: 2rem;
+  animation: ${(props) => (props.animate ? show : '')} 0.8s ease-in-out;
+`;
+export const Title3 = styled.h3`
+  color: ${Colors.redLigth};
+  font-size: 2rem;
+  animation: ${(props) => (props.animate ? show : '')} 1s ease-in-out;
+`;
+export const Title4 = styled.h4`
+  font-size: 0.8rem;
+  animation: ${(props) => (props.animate ? show : '')} 0.8s ease-in-out;
+`;
+export const Title5 = styled.h5`
+  font-size: 0.5rem;
 `;
