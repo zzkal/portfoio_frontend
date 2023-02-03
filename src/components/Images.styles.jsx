6c7fe6f1415c39234e398e8262379d7bf201471b
Andrés <lopez.andresrigoberto@gmail.com>
2023-02-03 17:45:35 -0600

    [edit] image

--- src/components/Images.styles.jsx
+++ src/components/Images.styles.jsx
@@ -2,22 +2,18 @@ import styled, { keyframes } from 'styled-components';
 
 const show = keyframes`
 from{
-  transform: translateY(10rem);
   opacity: 0;
 }
 to {
   opacity: 1;
-  transform: translateY(0);
 }
 `;
 
 export const MainImage = styled.img`
-  border-radius: 30px;
-  width: 15rem;
-  margin-top: 8rem;
-  align-self: center;
-  box-shadow: -7px 8px 21px -10px rgba(0, 0, 0, 0.69);
-  -webkit-box-shadow: -7px 8px 21px -10px rgba(0, 0, 0, 0.69);
-  -moz-box-shadow: -7px 8px 21px -10px rgba(0, 0, 0, 0.69);
-  animation: ${(props) => (props.animate ? show : '')} 0.8s ease-in-out;
+  width: 100%;
+  margin-top: 1rem;
+  box-shadow: -4px 3px 0px 1px rgba(0, 0, 0, 1);
+  -webkit-box-shadow: -4px 3px 0px 1px rgba(0, 0, 0, 1);
+  -moz-box-shadow: -4px 3px 0px 1px rgba(0, 0, 0, 1);
+  animation: ${show} 1s ease-in-out;
 `;
