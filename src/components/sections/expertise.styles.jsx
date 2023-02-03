978224ede68f3d89ae93cd81bd2b479c3d7c6b79
Andrés <lopez.andresrigoberto@gmail.com>
2023-02-03 17:48:40 -0600

    [add] styles

---
+++ src/components/sections/expertise.styles.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const ExpertiseSection = styled.div`
+  display: flex;
+  flex-flow: column;
+  justify-content: space-around;
+  padding: 8rem 4rem 4rem 4rem;
+`;
