1262e7d65fc222bd690812bb7e1d713958f77be6
Andrés <lopez.andresrigoberto@gmail.com>
2023-02-03 17:47:47 -0600

    [add] espertise section

--- src/routes/home/Home.component.jsx
+++ src/routes/home/Home.component.jsx
@@ -1,12 +1,14 @@
 import Presentation from '../../components/sections/Presentation.component';
 import About from '../../components/sections/About.component';
 import { HomePage } from './home.styles';
+import Expertise from '../../components/sections/Expertise.component';
 
 const Home = () => {
   return (
     <HomePage>
       <Presentation />
       <About />
+      <Expertise />
     </HomePage>
   );
 };
