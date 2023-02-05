ce2f7f3a7262504acb1b609ee202bb43dd4ae569
Andrés <lopez.andresrigoberto@gmail.com>
2023-02-05 13:54:18 -0600

    [add] accordion component

---
+++ src/components/accordion/AccordionItem.component.jsx
@@ -0,0 +1,101 @@
+import {
+  AccordionHeader,
+  PlusIcon,
+  AccordionDetails,
+  PlaceIcon,
+  LessIcon,
+} from './accordion.styles';
+import { Title3, Title4, Title5, Title6 } from '../Titles.component';
+import { Colors } from '../../styles/variables';
+import { List1 } from '../Lists.styled';
+import { TextWeight } from '../../styles/text-variables/text-weight';
+import { Fragment, useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+
+const AccordionItem = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpenAccordion = () => setIsOpen(!isOpen);
+
+  return (
+    <Fragment>
+      <AccordionHeader onClick={handleOpenAccordion}>
+        <Title3 weight={TextWeight.bold}>Facebook</Title3>
+        {isOpen ? <LessIcon /> : <PlusIcon />}
+      </AccordionHeader>
+      <AnimatePresence initial={false}>
+        {isOpen && (
+          <motion.div
+            key='content'
+            initial='collapsed'
+            animate='open'
+            exit='collapsed'
+            variants={{
+              open: { opacity: 1, height: 'auto' },
+              collapsed: { opacity: 0, height: 0 },
+            }}
+            transition={{ duration: 0.8, ease: [0.04, 0.62, 0.23, 0.98] }}
+            style={{ backgroundColor: Colors.blueDark }}
+          >
+            <AccordionDetails
+              variants={{ collapsed: { scale: 0.8 }, open: { scale: 1 } }}
+              transition={{ duration: 0.8 }}
+            >
+              <Title4 color={Colors.blueLight}>Facebook Co.</Title4>
+              <div style={{ marginTop: '3rem' }}>
+                <PlaceIcon />
+                <Title5
+                  color={Colors.blueLight}
+                  mt='2rem'
+                  weight={TextWeight.regular}
+                  size='1.75rem'
+                >
+                  USA
+                </Title5>
+              </div>
+              <Title6
+                color={Colors.blueLight}
+                weight={TextWeight.regular}
+                size='1.75rem'
+              >
+                2022 - present
+              </Title6>
+              <List1>
+                <li>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Mollitia illo,
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Quibusdam similique pariatur consectetur provident deleniti
+                  ipsum consequuntur fuga recusandae beatae, optio, sit
+                  voluptate,
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+                  ab quae, nesciunt unde doloribus temporibus impedit autem
+                  itaque
+                </li>
+              </List1>
+              <div style={{ marginTop: '3rem' }}>
+                <Title5 weight={TextWeight.bold} color={Colors.redLigth}>
+                  React
+                </Title5>
+                <Title5 weight={TextWeight.bold} color={Colors.redLigth}>
+                  MongoDb
+                </Title5>
+                <Title5 weight={TextWeight.bold} color={Colors.redLigth}>
+                  NextJS
+                </Title5>
+                <Title5 weight={TextWeight.bold} color={Colors.redLigth}>
+                  ExpressJS
+                </Title5>
+              </div>
+            </AccordionDetails>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </Fragment>
+  );
+};
+export default AccordionItem;
---
+++ src/components/accordion/accordion.styles.jsx
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+import { Colors } from '../../styles/variables';
+import { ReactComponent as PIcon } from '../../assets/more.svg';
+import { ReactComponent as PlIcon } from '../../assets/place.svg';
+import { ReactComponent as LIcon } from '../../assets/less.svg';
+import { motion } from 'framer-motion';
+import { scale, spin } from '../../styles/animation';
+
+export const AccordionContainer = styled.div`
+  margin-top: 11rem;
+`;
+
+export const AccordionHeader = styled.div`
+  background-color: ${Colors.blueLight};
+  color: ${Colors.blueDark};
+  border-bottom: 2px solid ${Colors.blueDark};
+  display: flex;
+  flex-flow: row;
+  justify-content: space-between;
+  align-items: center;
+  align-content: center;
+  height: 6rem;
+  width: 100%;
+  padding: 0 2rem 0 2rem;
+`;
+
+export const PlusIcon = styled(PIcon)`
+  height: 4rem;
+  width: 4rem;
+  animation: ${spin} 0.5s ease-in-out;
+`;
+
+export const LessIcon = styled(LIcon)`
+  height: 4rem;
+  width: 4rem;
+  animation: ${scale} 0.5s ease-in-out;
+`;
+
+export const PlaceIcon = styled(PlIcon)`
+  height: 3rem;
+  width: 3rem;
+  fill: ${Colors.blueLight};
+  margin-right: 2rem;
+`;
+
+export const AccordionDetails = styled(motion.div)`
+  display: flex;
+  flex-flow: column;
+  color: ${Colors.blueLight};
+  padding: 3rem;
+`;
