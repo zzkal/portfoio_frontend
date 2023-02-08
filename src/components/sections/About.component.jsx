d1ae5671762478eb66e54acf866bedd60f0583fc
Andrés <lopez.andresrigoberto@gmail.com>
2023-02-07 18:22:29 -0600

    [edit] new about me component

--- src/components/sections/About.component.jsx
+++ src/components/sections/About.component.jsx
@@ -1,7 +1,11 @@
-import { SectionContainer } from './about.styles';
+import { AboutMeSection, Divider } from './about.styles';
 import { Waypoint } from 'react-waypoint';
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 import Button from '../button/Button.component';
+import { Title1, Title2 } from '../Titles.component';
+import { Colors } from '../../styles/variables';
+import { Paragraph } from '../paragraphs.styles';
+import { TextSize } from '../../styles/text-variables/text-size';
 
 const About = () => {
   const [startAnimation, setStartAnimation] = useState(false);
@@ -12,9 +16,32 @@ const About = () => {
 
   return (
     <Waypoint onEnter={handleOnEnter}>
-      <SectionContainer>
-        {startAnimation && <Button text='About me' />}
-      </SectionContainer>
+      <AboutMeSection>
+        {startAnimation && (
+          <Fragment>
+            <Title1
+              color={Colors.redLigth}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.2 }}
+            >
+              About Me
+            </Title1>
+            <Paragraph
+              color={Colors.blueLight}
+              size={TextSize.small}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.2 }}
+            >
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi eos
+              iste quidem deserunt aut asperiores velit suscipit facilis,
+              corporis eum laborum iure optio recusandae quis autem est totam,
+              nam consectetur.
+            </Paragraph>
+          </Fragment>
+        )}
+      </AboutMeSection>
     </Waypoint>
   );
 };
--- src/components/sections/about.styles.jsx
+++ src/components/sections/about.styles.jsx
@@ -1,4 +1,6 @@
+import { motion } from 'framer-motion';
 import styled from 'styled-components';
+import { Colors } from '../../styles/variables';
 
 export const SectionContainer = styled.div`
   display: flex;
@@ -7,4 +9,15 @@ export const SectionContainer = styled.div`
   position: relative;
   padding: 0 2rem 10rem 2rem;
   margin-top: 8rem;
+`;
+
+export const AboutMeSection = styled(motion.section)`
+  display: flex;
+  clip-path: polygon(0 20%, 100% 0, 100% 80%, 0 100%);
+  flex-flow: column;
+  height: 100vh;
+  justify-content: center;
+  background-color: ${Colors.blueDark};
+  padding: 35% 2rem 35% 2rem;
+  margin-top: 20rem;
 `;
