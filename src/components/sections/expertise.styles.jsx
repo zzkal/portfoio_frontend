8c3ae051c541dceaaa0a1cceab51dc2354d43e35
Andrés <lopez.andresrigoberto@gmail.com>
2023-02-07 18:23:15 -0600

    [edit] add margin to expertise section

--- src/components/sections/expertise.styles.jsx
+++ src/components/sections/expertise.styles.jsx
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 export const ExpertiseSection = styled.div`
   display: flex;
   flex-flow: column;
+  height: 100vh;
   justify-content: space-around;
-  padding: 8rem 4rem 4rem 4rem;
+  padding: 0 2rem 0 2rem;
+  margin-top: 15rem;
 `;
