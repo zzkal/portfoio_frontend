1ddc3195896ce7bba8e740efce0f0d3811411a0e
Andrés <lopez.andresrigoberto@gmail.com>
2023-02-07 18:25:12 -0600

    [add] contact component

--- src/routes/home/Home.component.jsx
+++ src/routes/home/Home.component.jsx
@@ -4,6 +4,7 @@ import { HomePage } from './home.styles';
 import Expertise from '../../components/sections/Expertise.component';
 import Projects from '../../components/sections/Projects.component';
 import Job from '../../components/sections/Job.component';
+import Contact from '../../components/sections/contact.component';
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <Expertise />
       <Projects />
       <Job />
+      <Contact />
     </HomePage>
   );
 };
