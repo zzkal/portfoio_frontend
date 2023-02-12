c8e2f49dd35f8fd18d364c09a12eec78110e0051
Andrés <lopez.andresrigoberto@gmail.com>
2023-02-12 12:18:11 -0600

    [add] footer

---
+++ src/components/sections/Footer.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const FooterContainer = styled.footer`
+  background-color: black;
+  color: white;
+  text-align: center;
+  padding: 1rem;
+`;
+
+const Footer = () => {
+  return <FooterContainer>All rights reserved 2023</FooterContainer>;
+};
+
+export default Footer;
