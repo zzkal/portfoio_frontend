d48e034a3aecd08043ee9cd13fe82831e70e3f9c
Andrés <lopez.andresrigoberto@gmail.com>
2023-02-12 12:11:26 -0600

    [edit] list style

--- src/components/Lists.styled.jsx
+++ src/components/Lists.styled.jsx
@@ -1,6 +1,7 @@
 import styled, { keyframes } from 'styled-components';
 import { Colors } from '../styles/variables';
 import { show } from '../styles/animation';
+import { motion } from 'framer-motion';
 
 export const List1 = styled.ul`
   padding-top: 3rem;
@@ -10,5 +11,27 @@ export const List1 = styled.ul`
     font-weight: 400;
     list-style: inside;
     text-align: justify;
+  }
+`;
+
+export const ListMenuContainer = styled(motion.ul)`
+  display: flex;
+  flex-flow: column;
+  justify-content: start;
+  position: absolute;
+  top: 50vh;
+  left: 2rem;
+  list-style: none;
+  transform: translateY(-50%);
+`;
+
+export const ListMenuItem = styled(motion.li)`
+  display: block;
+  padding: 2rem;
+
+  & > a {
+    color: ${Colors.blueLight};
+    font-size: 2rem;
+    text-decoration: none;
   }
 `;
