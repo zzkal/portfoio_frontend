0d9d346ffd62054ce4669cea8c402ff0d9b4bee9
Andrés <lopez.andresrigoberto@gmail.com>
2023-02-12 12:18:26 -0600

    [delete] bg color

--- src/routes/home/home.styles.jsx
+++ src/routes/home/home.styles.jsx
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
-import { Colors } from '../../styles/variables.js';
 
 export const HomePage = styled.main`
-  //background-color: ${Colors.blueLight};
   display: flex;
   flex-direction: column;
   max-width: 100vw;
