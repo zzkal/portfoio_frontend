d0ada631d6b8faf256305725c9b934f06bd06c8f
Andrés <lopez.andresrigoberto@gmail.com>
2023-02-15 17:51:21 -0600

    [delete] form

--- src/components/sections/contact.component.jsx
+++ src/components/sections/contact.component.jsx
@@ -1,15 +1,9 @@
 import { Fragment, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { TextWeight } from '../../styles/text-variables/text-weight';
-import { Title1 } from '../Titles.component';
+import { Title1, Title4 } from '../Titles.component';
 import { Waypoint } from 'react-waypoint';
-import {
-  ButtonForm,
-  FormComponent,
-  InputComponent,
-  SelectComponent,
-  TextAreaComponent,
-} from '../form/form.styles';
+import Button from '../button/Button.component';
 import {
   ContactSection,
   IconBox,
@@ -18,7 +12,6 @@ import {
   GitHubIcon,
 } from './contact.styles';
 import { Colors } from '../../styles/variables';
-import { motion } from 'framer-motion';
 
 const Contact = () => {
   const [startAnimation, setStartAnimation] = useState(false);
@@ -27,7 +20,14 @@ const Contact = () => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm({
+    defaultValues: {
+      name: '',
+      email: '',
+      subject: '',
+      message: '',
+    },
+  });
 
   const onSubmit = (data) => console.log(data);
 
@@ -37,7 +37,7 @@ const Contact = () => {
 
   return (
     <Waypoint onEnter={handleOnEnter}>
-      <ContactSection>
+      <ContactSection id='contact'>
         {startAnimation && (
           <Fragment>
             <Title1
@@ -46,75 +46,16 @@ const Contact = () => {
               initial={{ y: 50, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
               transition={{ duration: 0.25 }}
+              mt='8rem'
             >
               Let's Work Together
             </Title1>
 
-            <FormComponent onSubmit={handleSubmit(onSubmit)}>
-              <InputComponent
-                placeholder='Name'
-                initial={{ y: 20, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                transition={{ duration: 0.25, delay: 0.25 }}
-                {...register(
-                  'name',
-                  { pattern: /^[A-Za-z]+$/i },
-                  { required: true },
-                  { min: 5 },
-                  { max: 50 }
-                )}
-              />
-
-              <InputComponent
-                placeholder='Email'
-                initial={{ y: 20, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                transition={{ duration: 0.25, delay: 0.3 }}
-                {...register(
-                  'email',
-                  {
-                    pattern:
-                      /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/,
-                  },
-                  { required: true },
-                  { max: 50 }
-                )}
-              />
-              <SelectComponent
-                initial={{ y: 20, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                transition={{ duration: 0.25, delay: 0.35 }}
-                {...register('subject')}
-              >
-                <option value='Asunto'>Asunto</option>
-                <option value='Asunto'>Asunto</option>
-                <option value='Asunto'>Asunto</option>
-              </SelectComponent>
-
-              <TextAreaComponent
-                placeholder='Message'
-                initial={{ y: 20, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                transition={{ duration: 0.25, delay: 0.4 }}
-                rows='5'
-                {...register(
-                  'message',
-                  { required: true },
-                  { min: 15 },
-                  { max: 150 }
-                )}
-              />
-
-              {errors.exampleRequired && <span>This field is required</span>}
-
-              <ButtonForm
-                type='submit'
-                initial={{ y: 20, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                transition={{ duration: 0.25, delay: 0.45 }}
-              />
-            </FormComponent>
-
+            <Title4 color={Colors.blueLight}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+              aut est dolorum.
+            </Title4>
+            <Button text='Email' style={{ marginTop: '6rem' }} />
             <IconContainer>
               <IconBox>
                 <LinkedInIcon
