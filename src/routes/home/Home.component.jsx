be8d0c12cc5b3d5dea24b8009d0a0dac6d79b0d8
Andrés <lopez.andresrigoberto@gmail.com>
2023-02-24 20:00:01 -0600

    [edit] loading api state

--- src/routes/home/Home.component.jsx
+++ src/routes/home/Home.component.jsx
@@ -1,4 +1,4 @@
-import { Fragment, useState, useEffect } from 'react';
+import { Fragment, useState, useEffect, useContext } from 'react';
 import Presentation from '../../components/sections/Presentation.component';
 import About from '../../components/sections/About.component';
 import { HomePage } from './home.styles';
@@ -9,20 +9,31 @@ import Contact from '../../components/sections/contact.component';
 import Footer from '../../components/sections/Footer.component';
 
 import data_portfolio from '../../data/data';
+import { DataContext } from '../../context/DataContext';
 
 const Home = () => {
+  const { isLoading } = useContext(DataContext);
+
   return (
-    <Fragment>
-      <HomePage>
-        <Presentation pdata={data_portfolio.profile} />
-        <About adata={data_portfolio.about} />
-        <Expertise edata={data_portfolio.expertise} />
-        <Projects projdata={data_portfolio.projects} />
-        <Job jdata={data_portfolio.workExperience} />
-        <Contact cdata={data_portfolio.contact} />
-      </HomePage>
-      <Footer />
-    </Fragment>
+    <>
+      {isLoading ? (
+        <>
+          <h1>Loading...</h1>
+        </>
+      ) : (
+        <Fragment>
+          <HomePage>
+            <Presentation />
+            <About />
+            <Expertise />
+            <Projects />
+            <Job />
+            <Contact />
+          </HomePage>
+          <Footer />
+        </Fragment>
+      )}
+    </>
   );
 };
 
