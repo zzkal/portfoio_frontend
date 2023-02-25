d0e96932cba05cbcf346b1e302490638ace8c19e
Andrés <lopez.andresrigoberto@gmail.com>
2023-02-24 20:40:24 -0600

    [edit] inject project data

--- src/routes/project-details/ProjectDetailsPage.component.jsx
+++ src/routes/project-details/ProjectDetailsPage.component.jsx
@@ -9,8 +9,6 @@ import { Colors } from '../../styles/variables';
 import Contact from '../../components/sections/contact.component';
 import Footer from '../../components/sections/Footer.component';
 import { TextWeight } from '../../styles/text-variables/text-weight';
-import Image1 from '../../assets/img/project.jpg';
-import { List2 } from '../../components/Lists.styled';
 import { LinkComponent } from '../../components/Links.styles';
 import { HomePage } from '../home/home.styles';
 import { useParams } from 'react-router-dom';
@@ -21,21 +19,39 @@ import {
 } from './projectDetailsContainer.styles';
 import { ProjectImage } from '../../components/Images.styles';
 import { DataContext } from '../../context/DataContext';
+import { List2 } from '../../components/Lists.styled';
 
 const ProjectDetailsPage = () => {
   const { pid } = useParams();
   const { projects } = useContext(DataContext);
   const [index, setIndex] = useState(parseInt(pid));
+  const [isLoading, setIsLoading] = useState(false);
   const [projectObject, setProjectObject] = useState({});
+  const [stackArray, setStackArray] = useState([]);
 
   useEffect(() => {
+    let arr = [];
+
     const fetchProject = async () => {
-      const response = await fetch(
-        `https://www.andres-dev-portfolio.site/api/projects/${index}`
-      );
-      const data = await response.json();
-      setProjectObject(data);
+      try {
+        setIsLoading(true);
+        const response = await fetch(
+          `https://www.andres-dev-portfolio.site/api/projects/${index}`
+        );
+        const data = await response.json();
+
+        data.stack.map(async (route) => {
+          const res = await fetch('https://andres-dev-portfolio.site' + route);
+          const data = await res.json();
+          arr.push(data.name);
+          setStackArray(arr);
+        });
+
+        setProjectObject(data);
+        setIsLoading(false);
+      } catch (error) {}
     };
+
     fetchProject();
 
     window.scrollTo(0, 0);
@@ -44,49 +60,61 @@ const ProjectDetailsPage = () => {
   return (
     <Fragment>
       <HomePage>
-        <ProjectDetailsContainer id='project-title'>
-          {window.innerWidth < 1200 ? (
-            <Fragment>
-              <Title1 color={Colors.blueDark} weight={TextWeight.bold}>
-                {projectObject.name}
-              </Title1>
-              <ProjectImage src={Image1} alt='img' />
-            </Fragment>
-          ) : (
-            <HeaderProjectImage imgUrl={Image1}>
-              <TitleProjects color={Colors.blueLight} weight={TextWeight.bold}>
-                {projectObject.name}
-              </TitleProjects>
-            </HeaderProjectImage>
-          )}
+        {isLoading ? (
+          <></>
+        ) : (
+          <ProjectDetailsContainer id='project-title'>
+            {window.innerWidth < 1200 ? (
+              <Fragment>
+                <Title1 color={Colors.blueDark} weight={TextWeight.bold}>
+                  {projectObject.name}
+                </Title1>
+                <ProjectImage
+                  src={`https://www.andres-dev-portfolio.site${projectObject.imgUrl}`}
+                  alt='img'
+                />
+              </Fragment>
+            ) : (
+              <HeaderProjectImage
+                imgUrl={`https://www.andres-dev-portfolio.site${projectObject.imgUrl}`}
+              >
+                <TitleProjects
+                  color={Colors.blueLight}
+                  weight={TextWeight.bold}
+                >
+                  {projectObject.name}
+                </TitleProjects>
+              </HeaderProjectImage>
+            )}
 
-          <Title3 weight={TextWeight.bold} mt='4rem'>
-            Description
-          </Title3>
-          <Paragraph>{projectObject.longDesc}</Paragraph>
-          <LinkComponent href={projectObject.repoLink}>Github</LinkComponent>
-          <LinkComponent href={projectObject.appLink}>Live</LinkComponent>
-          <Title3 weight={TextWeight.bold} mt='4rem'>
-            Type of project
-          </Title3>
-          <Paragraph>{projectObject.type}</Paragraph>
-          <Title3 weight={TextWeight.bold} mt='4rem'>
-            Stack
-          </Title3>
-          {/* <List2>
-            {stack.map((item) => (
-              <li key={item.id}>{item.skillName}</li>
-            ))}
-          </List2> */}
-          <Title3 weight={TextWeight.bold} mt='4rem'>
-            Design Desicions
-          </Title3>
-          <Paragraph>{projectObject.desicionDesc}</Paragraph>
-          <Title3 weight={TextWeight.bold} mt='4rem'>
-            Challenges and resolutions
-          </Title3>
-          <Paragraph>{projectObject.challengeDesc}</Paragraph>
-        </ProjectDetailsContainer>
+            <Title3 weight={TextWeight.bold} mt='4rem'>
+              Description
+            </Title3>
+            <Paragraph>{projectObject.longDesc}</Paragraph>
+            <LinkComponent href={projectObject.repoLink}>Github</LinkComponent>
+            <LinkComponent href={projectObject.appLink}>Live</LinkComponent>
+            <Title3 weight={TextWeight.bold} mt='4rem'>
+              Type of project
+            </Title3>
+            <Paragraph>{projectObject.type}</Paragraph>
+            <Title3 weight={TextWeight.bold} mt='4rem'>
+              Stack
+            </Title3>
+            <List2>
+              {stackArray.map((item) => (
+                <li key={stackArray.indexOf(item)}>{item}</li>
+              ))}
+            </List2>
+            <Title3 weight={TextWeight.bold} mt='4rem'>
+              Design Desicions
+            </Title3>
+            <Paragraph>{projectObject.desicionDesc}</Paragraph>
+            <Title3 weight={TextWeight.bold} mt='4rem'>
+              Challenges and resolutions
+            </Title3>
+            <Paragraph>{projectObject.challengeDesc}</Paragraph>
+          </ProjectDetailsContainer>
+        )}
 
         <Contact />
       </HomePage>
