b473a9cef8b030e15e2195daa9310e206c96affa
Andrés <lopez.andresrigoberto@gmail.com>
2023-02-24 19:49:23 -0600

    [edit] make responsive and inject profile data from api

--- src/components/sections/Presentation.component.jsx
+++ src/components/sections/Presentation.component.jsx
@@ -1,52 +1,112 @@
 import { MainImage } from '../Images.styles';
-import { PresentationContainer } from './presentation.styles';
+import {
+  PresentationContainer,
+  PresentatioItemsContainer,
+  PresentationTextContainer,
+} from './presentation.styles';
 import { Title1, Title4 } from '../Titles.component';
 import { Colors } from '../../styles/variables';
 import { TextWeight } from '../../styles/text-variables/text-weight';
 import { Paragraph } from '../paragraphs.styles';
 
 import myImage from '../../assets/img/man_portrait.jpeg';
+import { Fragment, useState, useEffect, useContext } from 'react';
+import { DataContext } from '../../context/DataContext';
 
-const Presentation = ({ pdata: { name, description, career, imgUrl } }) => {
+const Presentation = (props) => {
+  const { profile } = useContext(DataContext);
+
   return (
     <PresentationContainer>
-      <Title1
-        color={Colors.blueDark}
-        weight={TextWeight.bold}
-        initial={{ y: 100, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.5 }}
-      >
-        {name}
-      </Title1>
+      <PresentatioItemsContainer>
+        {window.innerWidth < 1200 ? (
+          <Fragment>
+            <Title1
+              color={Colors.blueDark}
+              weight={TextWeight.bold}
+              initial={{ y: 100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.5 }}
+            >
+              {profile.name}
+            </Title1>
 
-      <Title4
-        color={Colors.blueDark}
-        weight={TextWeight.bold}
-        initial={{ y: 100, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.5 }}
-      >
-        {career}
-      </Title4>
-      <MainImage
-        src={myImage}
-        initial={{ scale: 0, opacity: 0 }}
-        animate={{ scale: 1, opacity: 1 }}
-        transition={{ duration: 0.5 }}
-      />
+            <Title4
+              color={Colors.blueDark}
+              weight={TextWeight.bold}
+              initial={{ y: 100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.5 }}
+            >
+              {profile.career}
+            </Title4>
 
-      <Paragraph
-        color={Colors.blueDark}
-        weight={TextWeight.regular}
-        initial={{ y: 100, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.5 }}
-        size='1.5rem'
-        pt='1rem'
-      >
-        {description}
-      </Paragraph>
+            <MainImage
+              src={myImage}
+              initial={{ scale: 0, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              transition={{ duration: 0.5 }}
+            />
+
+            <Paragraph
+              color={Colors.blueDark}
+              weight={TextWeight.regular}
+              initial={{ y: 100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.5 }}
+              size='1.5rem'
+              pt='1rem'
+            >
+              {profile.description}
+            </Paragraph>
+          </Fragment>
+        ) : (
+          <Fragment>
+            <PresentationTextContainer>
+              <Title1
+                color={Colors.blueDark}
+                weight={TextWeight.bold}
+                initial={{ y: 100, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.5 }}
+                textalignleft='true'
+              >
+                {profile.name}
+              </Title1>
+
+              <Title4
+                size='5rem'
+                color={Colors.blueDark}
+                weight={TextWeight.bold}
+                initial={{ y: 100, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.5 }}
+              >
+                {profile.career}
+              </Title4>
+
+              <Paragraph
+                color={Colors.blueDark}
+                weight={TextWeight.regular}
+                initial={{ y: 100, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.5 }}
+                size='2.5rem'
+                pt='1rem'
+              >
+                {profile.description}
+              </Paragraph>
+            </PresentationTextContainer>
+
+            <MainImage
+              src={myImage}
+              initial={{ scale: 0, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              transition={{ duration: 0.5 }}
+            />
+          </Fragment>
+        )}
+      </PresentatioItemsContainer>
     </PresentationContainer>
   );
 };
--- src/components/sections/presentation.styles.jsx
+++ src/components/sections/presentation.styles.jsx
@@ -3,13 +3,47 @@ import styled from 'styled-components';
 export const PresentationContainer = styled.div`
   display: flex;
   flex-flow: column;
-  justify-content: center;
+  justify-content: start;
   position: relative;
   padding: 0 2rem 0 2rem;
   height: 100vh;
+
+  @media screen and (min-width: 700px) and (max-width: 1200px) {
+    height: 70vh;
+  }
+  @media screen and (min-width: 1200px) {
+    display: flex;
+    flex-flow: row;
+    justify-content: center;
+    //align-items: center;
+    padding: 8rem;
+  }
 `;
 
-export const TitleContainer = styled.div`
-  text-align: start;
-  margin-top: 10em;
+export const PresentatioItemsContainer = styled.div`
+  margin-top: 8rem;
+
+  @media (min-height: 700px) and (max-height: 900px) {
+    margin-top: 12rem;
+  }
+
+  @media screen and (min-width: 700px) and (max-width: 1200px) {
+    height: 50vh;
+    margin-top: 15rem;
+    padding-left: 4rem;
+    padding-right: 4rem;
+  }
+
+  @media screen and (min-width: 1200px) {
+    display: flex;
+    flex-flow: row;
+    justify-content: space-around;
+    min-width: 100%;
+  }
+`;
+
+export const PresentationTextContainer = styled.div`
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
 `;
