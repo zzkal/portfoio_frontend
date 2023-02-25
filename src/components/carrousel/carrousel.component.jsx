f0efa78c760c21f07104db3f30eb14573b8b9212
Andrés <lopez.andresrigoberto@gmail.com>
2023-02-24 19:57:44 -0600

    [edit] make responsive carrousel and inject projects data

--- src/components/carrousel/carrousel.component.jsx
+++ src/components/carrousel/carrousel.component.jsx
@@ -1,26 +1,63 @@
-import { useEffect } from 'react';
+import { Fragment, useEffect } from 'react';
 import { register } from 'swiper/element/bundle';
 import Card from '../card/Card.component';
 
 import './carrousel.styles.scss';
 
-const Carrrousel = ({ projdata }) => {
+const Carrrousel = ({ projects }) => {
   useEffect(() => register(), []);
 
-  return (
-    <swiper-container
-      class='swiper-style'
-      space-between='50'
-      loop='true'
-      pagination='true'
-    >
-      {projdata.map((project) => (
-        <swiper-slide>
-          <Card key={project.id} project={project} />
-        </swiper-slide>
-      ))}
-    </swiper-container>
-  );
+  const displayCarousel = () => {
+    if (window.innerWidth < 700) {
+      return (
+        <swiper-container
+          class='swiper-style'
+          loop='true'
+          pagination='true'
+          space-between='50'
+        >
+          {projects.map((project) => (
+            <swiper-slide>
+              <Card key={project.id} project={project} />
+            </swiper-slide>
+          ))}
+        </swiper-container>
+      );
+    } else if (window.innerWidth > 700 && window.innerWidth < 1200) {
+      return (
+        <swiper-container
+          class='swiper-style'
+          loop='true'
+          slides-per-view='2'
+          pagination='true'
+          space-between='50'
+        >
+          {projects.map((project) => (
+            <swiper-slide>
+              <Card key={project.id} project={project} />
+            </swiper-slide>
+          ))}
+        </swiper-container>
+      );
+    } else if (window.innerWidth > 1200) {
+      return (
+        <swiper-container
+          class='swiper-style'
+          slides-per-view='3'
+          loop='true'
+          pagination='true'
+        >
+          {projects.map((project) => (
+            <swiper-slide>
+              <Card key={project.id} project={project} />
+            </swiper-slide>
+          ))}
+        </swiper-container>
+      );
+    }
+  };
+
+  return <Fragment>{displayCarousel()}</Fragment>;
 };
 
 export default Carrrousel;
